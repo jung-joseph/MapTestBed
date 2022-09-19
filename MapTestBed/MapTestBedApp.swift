76ac79a0f3f5c3d99e2cd1697d2afb98a1316a03
Joseph Jung <joseph.jung@icloud.com>
2022-09-18 20:10:00 -0600

    Initial Commit

---
+++ MapTestBed/MapTestBedApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapTestBedApp.swift
+//  MapTestBed
+//
+//  Created by Joseph Jung on 9/18/22.
+//
+
+import SwiftUI
+
+@main
+struct MapTestBedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
